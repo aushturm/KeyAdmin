14340874abd7a6eecdedf97bf54c6f653505ce56
aushturm <scheisskontos@gmail.com>
2016-12-08 15:11:31 +0100

    start: ui admission control

--- KeyAdmin/ViewModel/Controller_MainWindow.cs
+++ KeyAdmin/ViewModel/Controller_MainWindow.cs
@@ -4,13 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using KeyAdmin.UI_Pages;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight;
+using System.ComponentModel;
 
 namespace KeyAdmin.ViewModel
 {
     public class Controller_MainWindow
     {
+        //private members
         private UserControl _display_view;
 
+        //properties
         public UserControl Displayed_View
         {
             get
@@ -20,7 +26,27 @@ namespace KeyAdmin.ViewModel
             set
             {
                 _display_view = value;
+                OnPropertyChanged("Displayed_View");
             }
+        }
+
+        //events
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //event handlers
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        //constuctors
+        public Controller_MainWindow()
+        {
+            UI_Admission_Control ui_adms_ctrl = new UI_Admission_Control();
+            Displayed_View = ui_adms_ctrl;
         }
 
 
