9c395f0e24ca4b6b0b853a83cdda28ec38fc9194
aushturm <scheisskontos@gmail.com>
2017-01-20 05:08:56 +0100

    Work on main page

---
+++ KeyAdmin/Model/DataHolder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyAdmin.Model
+{
+    public class AccountPropertiesItem
+    {
+        public string identifier;
+        public string value;
+    }
+
+    public class AccountItem
+    {
+        public string identifier;
+        public AccountPropertiesItem properties;
+    }
+}
--- KeyAdmin/ViewModel/Controller_UI_Main_Page.cs
+++ KeyAdmin/ViewModel/Controller_UI_Main_Page.cs
@@ -5,22 +5,61 @@ using System.Text;
 using System.Threading.Tasks;
 using KeyAdmin.Interfaces;
 using KeyAdmin.EventArgs;
+using KeyAdmin.Model;
+using System.ComponentModel;
 
 namespace KeyAdmin.ViewModel
 {
-    class Controller_UI_Main_Page : IUIPages
+    class Controller_UI_Main_Page : IUIPages, INotifyPropertyChanged
     {
-        #region events
-        public event EventHandler<ViewStateChangedEventArgs> ViewStateChanged;
+        #region members
+        private List<AccountItem> _accountItems = new List<AccountItem>();
         #endregion
 
-        #region raise event handler
+        #region properties
+        public List<AccountItem> AccountItems
+        {
+            get
+            {
+                return _accountItems;
+            }
+            set
+            {
+                _accountItems = value;
+                OnPropertyChanged("AccountItems");
+            }
+        }
+        #endregion
+
+        #region constructors
+        public Controller_UI_Main_Page()
+        {
+            AccountPropertiesItem properiesss = new AccountPropertiesItem() { identifier = "propertie identifier", value = "propertie value" };
+            AccountItem accountItem = new AccountItem() { identifier = "accountItem identifier", properties = properiesss };
+            AccountItems.Add(accountItem);
+        }
+        #endregion
+
+        #region events
+        public event EventHandler<ViewStateChangedEventArgs> ViewStateChanged;
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region raise event handlers
         protected void OnViewStateChanged(ViewStateChangedEventArgs e)
         {
             EventHandler<ViewStateChangedEventArgs> handler = ViewStateChanged;
             if (handler != null)
             {
                 handler(this, e);
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
             }
         }
         #endregion
